379830d0baa46d8e7a6852128b6d77f4943338c9
odang3A <odang2923@naver.com>
2020-07-26 22:44:01 +0900

    fire mode

--- Assets/Scripts/GunControl.cs
+++ Assets/Scripts/GunControl.cs
@@ -4,34 +4,35 @@ using UnityEngine;
 
 public class GunControl : MonoBehaviour
 {
-    // gun type
+    // gun type, info
     string gunName;
-    Vector3 hipLoc;
-    Vector3 aimLoc;
-    float damage;
-    float fireRate;
-    bool isSelectiveFire;
-    int capacity;
+    Vector3 hipLoc;     // hip location
+    Vector3 aimLoc;     // aim location
+    float damage;       // damage amount (knockback force)
+    float fireRate;     // fire rate
+    bool isSelectiveFire;   // is selective fire
+    int capacity;       // capacity
+    int currAmmo;       // current ammo amount
 
-    bool isEquip = false;
-    bool isFireable = false;
-    int fireMode;   // -1: single, 1: auto
-    int currAmmo;
-    float nextTimeToFire = 0f;
-    Transform FPSCamera; // FPSCamera
-    HUDControl hudControl;
-    BoxCollider[] gunColliders;
-    Rigidbody gunRb;
+    bool isEquip = false;   // is equiped
+    bool isFireable = false;    // is fireable
+    int fireMode;       // fire mode    // -1: single, 1: auto
+    int currMag;        // current bullets in mag
+    float nextTimeToFire = 0f;  // time to fire
+    Transform FPSCamera; // FPSCamera (players eye)
+    HUDControl hudControl;  // HUD
+    BoxCollider[] gunColliders; // guns colliders
+    Rigidbody gunRb;    // guns rigidbody
 
     // effects
-    public ParticleSystem muzzleFlash;
-    public GameObject hitImpact;
+    public ParticleSystem muzzleFlash;  // muzzle flash effect
+    public GameObject hitImpact;        // hit impact effect
 
 
     void Start()
     {
-        gunColliders = GetComponents<BoxCollider>();
-        gunRb = GetComponent<Rigidbody>();
+        gunColliders = GetComponents<BoxCollider>();    // get guns colliders
+        gunRb = GetComponent<Rigidbody>();              // get guns rigidbody
     }
 
     // Update is called once per frame
@@ -39,19 +40,19 @@ public class GunControl : MonoBehaviour
     {
         if (isEquip)
         {
-            control();
+            control();  // main control of the guns (fire, aim, fire mode)
 
-            // 실행순서 주의할것 // tlakrf
+            // 실행순서 주의할것
 
-            if (Input.GetKeyDown("x"))
-            {   // weapon down
-                weaponDown();
+            if (Input.GetKeyDown("x"))      // unequip guns
+            {
+                unequip();
             }
 
-            if(Input.GetKeyDown("r") || currAmmo <= 0)
+            if(Input.GetKeyDown("r") || currMag <= 0)
             {
                 //StartCoroutine(reload());
-                weaponDown();
+                unequip();
                 Debug.Log("tlqkf");
             }
 
@@ -60,87 +61,98 @@ public class GunControl : MonoBehaviour
 
     void control()
     {
-        if (Input.GetKeyDown("b") && isSelectiveFire)
-        {   // change fire mode
-            fireMode *= -1;
+        if (Input.GetKeyDown("b") && isSelectiveFire)   // change fire mode
+        {
+            fireMode *= -1;     // -1: single, 1: auto
+            hudControl.changeFireMode(fireMode);    // update fire mode HUD
         }
 
         switch (fireMode)   // check fire mode
         {   // -1: single, 1: auto
             case -1:
-                isFireable = Input.GetMouseButtonDown(0);
+                isFireable = Input.GetMouseButtonDown(0);   // single
                 break;
             case 1:
-                isFireable = Input.GetMouseButton(0) && Time.time >= nextTimeToFire;
+                isFireable = Input.GetMouseButton(0) && Time.time >= nextTimeToFire;    // auto
                 break;
             default:
                 break;
         }
 
 
-        if (isFireable && currAmmo > 0)
-        {   // fire
+        if (isFireable && currMag > 0)   // fire
+        {
             fire();
         }
 
 
         // aim
-        transform.localPosition = (Input.GetMouseButton(1) ? aimLoc : hipLoc);
-        FPSCamera.GetComponent<Camera>().fieldOfView = (transform.localPosition == aimLoc ? 30f : 60f);
-        hudControl.hideCrosshair(transform.localPosition == aimLoc);
+        aim(Input.GetMouseButton(1));   // set aim
     }
 
     void fire()
     {
-        currAmmo--;
-        nextTimeToFire = Time.time + 1f / fireRate;
+        currMag--;  // 
+        hudControl.updateAmmo(currMag, currAmmo);   // update Ammo HUD
+
+        nextTimeToFire = Time.time + 1f / fireRate;     // fire rate
 
         muzzleFlash.Play(); // play muzzle flash effects
 
         RaycastHit hit;
-        if (Physics.Raycast(FPSCamera.position, FPSCamera.forward, out hit))
-        {   // if hit something
-            Debug.Log("We hit " + hit.collider.name + ": " + hit.point);
+        if (Physics.Raycast(FPSCamera.position, FPSCamera.forward, out hit))   // if hit something
+        {
+            Debug.Log("We hit " + hit.collider.name + ": " + hit.point);    // hit log
 
-            if (hit.rigidbody)
-            {   // knockback
+            if (hit.rigidbody)   // knockback
+            {
                 //hit.rigidbody.AddExplosionForce(damage, hit.point, 5, 0.2f, ForceMode.Impulse);
-                hit.rigidbody.AddForce(transform.forward * damage, ForceMode.Impulse);
+                hit.rigidbody.AddForce(transform.forward * damage, ForceMode.Impulse);  // knockback by damage
             }
 
             // play hit Impact effects
-            GameObject impactObj = Instantiate(hitImpact, hit.point, Quaternion.LookRotation(hit.normal));
-            Destroy(impactObj, 1);
+            GameObject impactObj = Instantiate(hitImpact, hit.point, Quaternion.LookRotation(hit.normal));  // instantiate hit impact effect
+            Destroy(impactObj, 1);  // destroy hit impact effect
         }
     }
 
-    IEnumerator reload()
+    void aim(bool isAimed)  // aim
+    {   
+        transform.localPosition = (isAimed ? aimLoc : hipLoc);  // gun to aim location
+        FPSCamera.GetComponent<Camera>().fieldOfView = (isAimed ? 30f : 60f);   // zoom in
+        hudControl.showCrosshair(isAimed);  // disable crosshair HUD
+    }
+
+    IEnumerator reload()   // reload
     {
         yield return new WaitForSeconds(1);
     }
 
-    void weaponDown()
-    {   // weapon down
-        isEquip = false;
-        transform.parent = transform.root.parent;
-        foreach (BoxCollider gunCollider in gunColliders)
+    void unequip()   // unequip guns
+    {
+        aim(false); // disable aim
+        isEquip = false;    // unequip
+        hudControl.showGunInfo(isEquip);    // disable guninfo UHD
+        transform.parent = transform.root.parent;   // set parent to null
+        foreach (BoxCollider gunCollider in gunColliders)   // enable colliders
         {
             gunCollider.enabled = true;
         }
-        gunRb.useGravity = true;
+        gunRb.useGravity = true;    // set physics
     }
 
     public void SetGunType(string name, Vector3 hipLoc, Vector3 aimLoc, float damage, float fireRate, bool isSelectiveFire, int defaultFireMode, int capacity)
     {
-        gunName = name;
-        this.hipLoc = hipLoc;
-        this.aimLoc = aimLoc;
-        this.damage = damage;
-        this.fireRate = fireRate;
-        this.isSelectiveFire = isSelectiveFire;
-        fireMode = defaultFireMode;
-        this.capacity = capacity;
-        currAmmo = this.capacity + 1;
+        gunName = name;         // name
+        this.hipLoc = hipLoc;   // hip location
+        this.aimLoc = aimLoc;   // aim location
+        this.damage = damage;   // damage (knockback force)
+        this.fireRate = fireRate;   // fire rate
+        this.isSelectiveFire = isSelectiveFire; // is selective fire
+        fireMode = defaultFireMode;     // default fire mode
+        this.capacity = capacity;       // gun capacity
+        currAmmo = this.capacity + 1;   // default ammo
+        currMag = this.capacity + 1;    // fill mag
     }
 
 
@@ -150,15 +162,22 @@ public class GunControl : MonoBehaviour
         FPSCamera = selector;
         hudControl = FPSCamera.parent.Find("HUD").GetComponent<HUDControl>();
 
+        // disable colliders
         foreach (BoxCollider gunCollider in gunColliders)
         {
             gunCollider.enabled = false;
         }
-        
+
+        // set gun location, rotation, physics
         transform.SetParent(FPSCamera);
         transform.localPosition = hipLoc;
         transform.localRotation = Quaternion.identity;
         gunRb.angularVelocity = Vector3.zero;
         gunRb.useGravity = false;
+
+        // update HUD
+        hudControl.showGunInfo(isEquip);        // show gun info UHD
+        hudControl.updateAmmo(currMag, currAmmo);   // update ammo HUD
+        hudControl.changeFireMode(fireMode);    // set fire mode HUD
     }
 }
--- Assets/Scripts/HUDControl.cs
+++ Assets/Scripts/HUDControl.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 
 public class HUDControl : MonoBehaviour
 {
-    public UnityEngine.UI.Image Crosshair;
+    public UnityEngine.UI.Image Crosshair;  // crosshair image
+    public Text Ammo;       // ammo
+    public Text FireMode;   // fire mode
 
     // Start is called before the first frame update
     void Start()
@@ -14,21 +16,35 @@ public class HUDControl : MonoBehaviour
         
     }
 
-    public void hideCrosshair(bool isAimed)
+    public void updateAmmo(int currMag, int currAmmo)   // update ammo
     {
-        Crosshair.enabled = !isAimed;
+        Ammo.text = "" + currMag + "/" + currAmmo;
     }
 
-    public void changeFireMode(int mode)
+    public void changeFireMode(int mode)    // change fire mode
     {
-        switch (mode)
-        {   // -1: single, 1: auto
+        switch (mode)   // -1: single, 1: auto
+        {
             case -1:
+                FireMode.text = "SINGLE";
                 break;
             case 1:
+                FireMode.text = "AUTO";
                 break;
             default:
                 break;
         }
     }
+
+    public void showGunInfo(bool isEquip)   // show gun info if equiped (ammo, fire mode)
+    {
+        Ammo.enabled = isEquip;
+        FireMode.enabled = isEquip;
+    }
+
+    public void showCrosshair(bool isAimed) // show crosshair if not aiming
+    {
+        Crosshair.enabled = !isAimed;
+    }
+
 }
